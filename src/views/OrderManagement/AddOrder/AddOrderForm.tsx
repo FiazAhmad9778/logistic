20518026f47bc6f95e1a282afd22c785f42d8d35
Muhammad Zeeshan <iamzeeshan.dev@gmail.com>
2023-07-24 22:31:00 +0500

    Client Dropdown permission:

--- src/views/OrderManagement/AddOrder/AddOrderForm.tsx
+++ src/views/OrderManagement/AddOrder/AddOrderForm.tsx
@@ -1,9 +1,11 @@
 import Button from '@/components/Button';
 import Form from '@/components/Form';
+import PermissionWrapper from '@/components/Permission/permission-helper';
 import { ClientResponse } from '@/infrastructure/store/api/client/client-types';
 import { RouteResponse } from '@/infrastructure/store/api/route/route-types';
 import { Row, Col } from 'react-bootstrap';
 import { FieldValues, SubmitHandler, UseFormReturn } from 'react-hook-form';
+import { ClaimCode } from 'src/enums/claim-codes';
 
 interface IAddOrderForm {
   onSubmit: SubmitHandler<FieldValues>;
@@ -29,16 +31,18 @@ const AddOrderForm: React.FC<IAddOrderForm> = ({ useFormReturn, onSubmit, routes
                 }))}
               />
             </Col>
-            <Col xl={6} lg={6} md={6} sm={12}>
-              <Form.Select
-                label="Client"
-                name="clientId"
-                options={clients?.map((option) => ({
-                  value: option.id,
-                  name: option.name,
-                }))}
-              />
-            </Col>
+            <PermissionWrapper requiredClaims={[ClaimCode.CliMgA]}>
+              <Col xl={6} lg={6} md={6} sm={12}>
+                <Form.Select
+                  label="Client"
+                  name="clientId"
+                  options={clients?.map((option) => ({
+                    value: option.id,
+                    name: option.name,
+                  }))}
+                />
+              </Col>
+            </PermissionWrapper>
             <Col xl={6} lg={6} md={6} sm={12}>
               <Form.Input
                 label="Unique Reference Code"
