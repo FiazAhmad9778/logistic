e1b74a96a3266f0f5278b83958b16581f0beb66b
FiazAhmad9778 <fiazahmad1g@gmail.com>
2023-07-30 23:42:22 +0500

    fixed

--- src/form-resolver/safety-check/safety-check-resolver.ts
+++ src/form-resolver/safety-check/safety-check-resolver.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 const genericResolver = {
   checkName: yup.string().required('Check name is required!'),
   checkDescription: yup.string().required('Description is required!'),
-  fieldTypeId: yup.string().required('Field type is required!'),
+  fieldType: yup.string().required('Field type is required!'),
   checkActive: yup.boolean(),
   isNumberOnly: yup.boolean(),
   isMajorFault: yup.boolean(),
@@ -12,7 +12,7 @@ const genericResolver = {
 export const addSafetyCheckResolver = yup.object().shape({
   checkName: genericResolver.checkName,
   checkDescription: genericResolver.checkDescription,
-  fieldTypeId: genericResolver.fieldTypeId,
+  fieldType: genericResolver.fieldType,
   checkActive: genericResolver.checkActive,
   isNumberOnly: genericResolver.isNumberOnly,
   isMajorFault: genericResolver.isMajorFault,
